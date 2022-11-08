91343c2c74bfef00dd798e12df6c04d2a69daf18
hongfeizsy <hongfeizsy@gmail.com>
2022-11-09 00:18:22 +0100

    Add background and create the folder of cards.

---
+++ YangLeGeYang_V1/Assets/Game/Script/Card.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("Mouse down.");
+    }
+}
