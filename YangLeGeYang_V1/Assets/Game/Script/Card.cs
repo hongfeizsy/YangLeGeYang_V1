5652102c50a8476d7c4fd033987a2e0a8c04ef23
hongfeizsy <hongfeizsy@gmail.com>
2022-11-11 17:18:37 +0100

    Card will fly to destination once mouse is down.

--- YangLeGeYang_V1/Assets/Game/Script/Card.cs
+++ YangLeGeYang_V1/Assets/Game/Script/Card.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+
 
 public class Card : MonoBehaviour
 {
+    [SerializeField] CardDestination cardDestination;
+
     private void OnMouseDown()
     {
-        Debug.Log("Mouse down.");
+        Vector2 destinationPos = cardDestination.transform.position;
+        transform.DOMove(destinationPos, 1);
     }
 }
