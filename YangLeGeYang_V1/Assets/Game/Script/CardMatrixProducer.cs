e8b68d6e5e7565cb36bde4384fd3c0c0e0d48b5b
hongfeizsy <hongfeizsy@gmail.com>
2022-12-29 12:06:23 +0100

    Create card matrix producer.

---
+++ YangLeGeYang_V1/Assets/Game/Script/CardMatrixProducer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMatrixProducer : MonoBehaviour
+{
+    [SerializeField] Card[] CardPrefabs;
+    [SerializeField] int LayerNumber;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
