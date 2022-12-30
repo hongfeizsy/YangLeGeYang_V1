61c0376c69152d480651dfcb81216dbf535f0ea6
hongfeizsy <hongfeizsy@gmail.com>
2022-12-30 12:00:23 +0100

    Able to create multiple layers with arrangements.

--- YangLeGeYang_V1/Assets/Game/Script/CardMatrixProducer.cs
+++ YangLeGeYang_V1/Assets/Game/Script/CardMatrixProducer.cs
@@ -22,23 +22,33 @@ public class CardMatrixProducer : MonoBehaviour
         row = 7;
         column = 5;
         float shiftInZAxis = 0.01f;
+        System.Random rnd = new System.Random(123);
         for (int k = 0; k < layer; k++) 
         {
             for (int j = 0; j < row; j++) 
             {
                 for (int i = 0; i < column; i++) 
                 {
-                    if (j % 2 == k % 2 && i % 2 == k % 2) 
+                    if (CreateFillingCondition(k, j, i) && rnd.Next(100) <= 70) 
                     {
                         var cardObject = Instantiate(CardPrefabs[k], 
                             new Vector3(((float)i - column / 2) * (cardWidth / 2) * cardScalingFactor, ((float)j - row / 2) * (cardHeight / 2) * cardScalingFactor, 
-                                -shiftInZAxis * k), Quaternion.identity, gameObject.transform);
+                                - shiftInZAxis * k), Quaternion.identity, gameObject.transform);
                         coordinate = new Vector3(i, j, k);
                         cardObject.GetComponent<Card>().Coordidate = coordinate;
                     }
                 }
             }
         }
+    }
+
+    private bool CreateFillingCondition(int layer, int col, int row) 
+    {
+        if (layer % 4 == 0 && col % 2 == 0 && row % 2 == 0) { return true; }
+        if (layer % 4 == 1 && col % 2 == 0 && row % 2 == 1) { return true; }
+        if (layer % 4 == 2 && col % 2 == 1 && row % 2 == 1) { return true; }
+        if (layer % 4 == 3 && col % 2 == 1 && row % 2 == 0) { return true; }
+        return false;
     }
 
     void Update()
