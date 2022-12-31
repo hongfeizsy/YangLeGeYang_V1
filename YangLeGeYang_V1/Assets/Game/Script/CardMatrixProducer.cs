13fd77dd61edc0f667fb67933cce9c482f97110b
hongfeizsy <hongfeizsy@gmail.com>
2022-12-31 16:00:31 +0100

    Working on simple case to check levels of cards.

--- YangLeGeYang_V1/Assets/Game/Script/CardMatrixProducer.cs
+++ YangLeGeYang_V1/Assets/Game/Script/CardMatrixProducer.cs
@@ -11,6 +11,8 @@ public class CardMatrixProducer : MonoBehaviour
     int row, column;
     bool[,,] occupiedIndicator;
     float cardWidth, cardHeight;
+    List<int> CardIndex = new List<int>();
+    List<Vector3> CoordidateList = new List<Vector3>();
 
     void Start()
     {
@@ -23,17 +25,22 @@ public class CardMatrixProducer : MonoBehaviour
         column = 5;
         float shiftInZAxis = 0.01f;
         System.Random rnd = new System.Random(123);
+        
         for (int k = 0; k < layer; k++) 
         {
+            // if (CoordidateList.Count < (k + 1)) { CoordidateList.Add(new List<Vector3>()); }
             for (int j = 0; j < row; j++) 
             {
                 for (int i = 0; i < column; i++) 
                 {
                     if (k % 2 == j % 2 && k % 2 == i % 2) 
                     {
+                        coordinate = new Vector3((int)(i - column / 2), (int)(j - row / 2), (int)k);
                         var cardObject = Instantiate(CardPrefabs[k],
-                            new Vector3(((float)i - column / 2) * (cardWidth / 2) * cardScalingFactor, ((float)j - row / 2) * (cardHeight / 2) * cardScalingFactor,
-                                -shiftInZAxis * k), Quaternion.identity, gameObject.transform);
+                            new Vector3(coordinate.x * (cardWidth / 2) * cardScalingFactor, coordinate.y * (cardHeight / 2) * cardScalingFactor,
+                            -shiftInZAxis * coordinate.z), Quaternion.identity, gameObject.transform);
+                        CoordidateList.Add(coordinate);
+                        cardObject.Coordidate = coordinate;
                     }
                     
                     // if (CreateFillingCondition(k, j, i) && rnd.Next(100) <= 70) 
@@ -47,6 +54,9 @@ public class CardMatrixProducer : MonoBehaviour
                 }
             }
         }
+        CardIndex = Enumerable.Range(0, CoordidateList.Count).ToList<int>();
+
+        IdentifyCardLevels(row, column, layer);
     }
 
     private bool CreateFillingCondition(int layer, int col, int row) 
@@ -58,8 +68,36 @@ public class CardMatrixProducer : MonoBehaviour
         return false;
     }
 
-    void Update()
+    private void IdentifyCardLevels(int row, int col, int layer) 
     {
-        
+        int _row = 0, _col = 0, _layer = 0;
+        List<List<int>> levels = new List<List<int>>(); 
+        foreach (Vector3 coordinate in CoordidateList)
+        {
+            print(coordinate);
+            if (coordinate.z == layer - 1) { levels.Add(new List<int>()); }
+            else 
+            {
+                _row = (int)coordinate.x;
+                _col = (int)coordinate.y;
+                _layer = (int)coordinate.z;
+
+                for (int k = _layer; k < layer; k++) 
+                {
+                    for (int j = _col - 1; j < _col + 2; j++)
+                    {
+                        for (int i = _row - 1; i < _row + 2; i++)
+                        {
+                            
+                        }
+                    }
+                }
+
+
+
+
+            }
+        }
+
     }
 }
